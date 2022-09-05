55fc8aa50c6e0dae897c6d78ce45d179e1d4893f
dlalswo222 <dlalswo222>
2022-09-05 20:22:37 +0900

    add count.cc

---
+++ count.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void count_(int num){
+	for(int i=1;i<=num;i++){
+		cout << i << endl;
+	}
+	cout << "Start!!" << endl;
+	cout << 2017313220 << " " << "이민재" << endl;
+	return;
+}
